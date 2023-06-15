000efb0f0ae84143a8abff19d56d7aa3d84709eb
shettyvinyas <vinyas1947@gmail.com>
2023-06-15 16:15:35 +0530

    Add files via upload
    This file contains the set up of react program

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
-/**
- * <div id="parent">
- *      <div id="child">
- *          <h1>I'm h1 tag</h1>
- *          <h2>I'm h2 tag</h2>
- *       </div>
- *   <div id="child2">
- *          <h1>I'm h1 tag</h1>
- *          <h2>I'm h2 tag</h2>
- *       </div>
- * </div>
- */
-const h1 = React.createElement("h1",{}, "I'm h1 tag");
-const h2 = React.createElement("h2",{}, "I'm h2 tag");
-const child1 = React.createElement("div", { id: "child12" }, [h1, h2])
-const child2 = React.createElement("div", { id: "child21" }, [h1, h2])
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" },
-    [child1, child2])
+const jsxheading=(<h1 className="Vinyas">Namaste Javascript</h1>)
 
+console.log(jsxheading);
 
-// const heading = React.createElement("h1", { id: "heading" }, "Hello World from React");
+const root=ReactDOM.createRoot(document.getElementById('root'))
 
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent)
+root.render(jsxheading);
